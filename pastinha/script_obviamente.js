27f509f5347bbe332fcc2fe75ea7c4e0b661d523
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2023-07-24 13:44:03 -0300

    Add files via upload

--- pastinha/script_obviamente.js
+++ pastinha/script_obviamente.js
@@ -30,14 +30,68 @@ function SelecionaM(){
 
 function Notas(){
 
-    let n1 = parseInt(document.getElementById("n1")).value
-    let n2 = parseInt(document.getElementById("n2")).value
-    let n3 = parseInt(document.getElementById("n3")).value
+    let n1 = parseFloat(document.getElementById("n1").value)
+
+    let n2 = parseFloat(document.getElementById("n2").value)
+
+    let n3 = parseFloat(document.getElementById("n3").value)
 
     let soma;
 
-    soma = (n1+n2+n3)/3
+    soma = (n1+n2+n3)/3;
 
     document.getElementById("Resposta").innerText = soma
+
+}
+
+function Comparar(){
+
+    let n1 = parseInt(document.getElementById("n1").value)
+
+    let n2 = parseInt(document.getElementById("n2").value)
+
+    if(n1 == n2){
+
+        document.getElementById("Resposta").innerText = "Os Dois Numeros são iguais"
+
+    }else{
+
+        document.getElementById("Resposta").innerText = "Os Dois Numeros são diferentes"
+
+    }
+
+    if(n1 === n2){
+
+        document.getElementById("Resposta").innerText = "Os Dois Numeros sao Estritamente iguais"
+    }else{
+
+        document.getElementById("Resposta").innerText = "Os Dois Numeros sao Estritamente Diferentes"
+
+    }
+
+
+}
+
+
+function Calcular(){
+
+    let n1 = parseFloat(document.getElementById("n1").value)
+    let n2 = parseFloat(document.getElementById("n2").value)
+    let n3 = parseFloat(document.getElementById("n3").value)
+
+    if(n1 == n2 && n1== n3 && n2 == n1 && n2 == n3 && n3 == n1 && n3 ==  n2){
+
+        document.getElementById("Resposta").innerText = "Triangulo Equilátero"
+
+    }else if(n1 != n2 && n1 != n3 && n2 != n1 && n2!= n3 && n3 != n1 && n3 != n2){
+
+        document.getElementById("Resposta").innerText = "Triangulo Escaleno"
+
+    }else{
+
+        document.getElementById("Resposta").innerText = "Triangulho Isosceles"
+
+    }
+
 
 }
