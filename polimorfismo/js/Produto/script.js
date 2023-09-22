434f51889afb9cd76b527ff4d8bd819a19ee4fe9
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2023-09-22 06:55:50 -0300

    Atualizar o script.js

--- polimorfismo/js/Produto/script.js
+++ polimorfismo/js/Produto/script.js
@@ -9,6 +9,7 @@ document.getElementById("enviar").addEventListener("click", function adicionarPr
     let preco = document.getElementById("preco").value;
     let dataFabricacao = new Date(document.getElementById("dataFabricacao").value).toLocaleDateString('en-GB');
     let dataValidade = new Date(document.getElementById("dataValidade").value).toLocaleDateString('en-GB');
+ let dataHoje = new Date().toLocaleDateString('en-GB');
 
     if (nome === '' || preco === '') {
 
@@ -18,7 +19,23 @@ document.getElementById("enviar").addEventListener("click", function adicionarPr
     } else {
 
 
-        let produto = new Produto(nome, preco, dataFabricacao, dataValidade);
+       if(dataValidade > dataHoje){
+      
+       let naoVencido = "Dentro da Data de Validade"
+
+          let produto = new Produto(nome, preco, dataFabricacao, dataValidade,naoVencido);
+
+        produtos.adicionarProduto(produto);
+
+        let lista = document.getElementById("listaProduto");
+        while (lista.firstChild) {
+            lista.removeChild(lista.firstChild)
+
+        }eles{
+
+          let vencido = "Fora da Data de Validade"  
+
+            let produto = new Produto(nome, preco, dataFabricacao, dataValidade,vencido);
 
         produtos.adicionarProduto(produto);
 
@@ -26,6 +43,19 @@ document.getElementById("enviar").addEventListener("click", function adicionarPr
         while (lista.firstChild) {
             lista.removeChild(lista.firstChild)
         }
+
+
+
+
+
+}
+
+
+
+       }
+
+
+  
 
 
     }
